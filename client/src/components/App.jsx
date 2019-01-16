6a654b5a988916cfaeed55bdde07c6b381e30bab
Ubuntu <ubuntu@ip-172-31-42-241.ec2.internal>
2019-01-16 17:28:39 +0000

    working

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,7 +13,7 @@ class App extends Component {
     this.state = {
       movieData: {}
     };
-    this.API_URL = 'http://ec2-52-201-21-135.compute-1.amazonaws.com/movie/';
+    this.API_URL = 'http://ec2-18-235-34-167.compute-1.amazonaws.com/movie/';
 //      process.env.NODE_ENV === 'production'
 //      ? process.env.API_URL
 //      : 'http://localhost:9002/movie/';
